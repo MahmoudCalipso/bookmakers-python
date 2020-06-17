16f0a1e83d2f87e57dc4605bdbb510105ec8fa59
Antonio Jesús Sánchez <antonio.sanchez@idbmobile.com>
2020-06-17 16:22:41 +0200

    Inserting bookmaker events

--- queues/Readers/1xbet/1592403651/0/bookmaker_markets.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO bookmaker_markets (id, fk_bookmaker_id, fk_bookmaker_sport_id, title, skip, outcomes, mapped, created_at) VALUES 
-(DEFAULT, 14, {sport=Cricket}, 'Double chance', 0, '["1X", "12", "2X"]', 0, '2020-06-17 00:00:00')
-,(DEFAULT, 14, {sport=Formula 1}, 'Driver To Win', 0, '["Red Bull", "Mercedes", "Ferrari", "McLaren", "Williams", "Renault", "Haas", "Alfa Romeo", "Racing Point", "Alpha Tauri", "Red Bull", "Mercedes", "Ferrari"]', 0, '2020-06-17 00:00:00')
