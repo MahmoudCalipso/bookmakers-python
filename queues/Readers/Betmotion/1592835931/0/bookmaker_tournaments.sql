24ced12853fefa77a9da9d074a60067b61579ef7
Antonio Jesús Sánchez <antonio.sanchez@idbmobile.com>
2020-06-22 16:27:34 +0200

    Starting Betmotion reader

--- queues/Readers/Betmotion/1592835931/0/bookmaker_tournaments.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO bookmaker_tournaments (id, fk_bookmaker_id, fk_bookmaker_sport_id, title, skip, mapped, created_at, found_in) VALUES 
-(DEFAULT, 17, {sport=Romania}, 'Liga I', 0, 0, '2020-06-22 00:00:00', 'FC Viitorul Constanta vs. AFC Hermannstadt')
-,(DEFAULT, 17, {sport=Italy}, 'Serie A', 0, 0, '2020-06-22 00:00:00', 'US Lecce vs. AC Milan')
