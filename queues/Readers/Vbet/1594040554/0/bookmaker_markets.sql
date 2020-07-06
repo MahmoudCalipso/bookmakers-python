1baeed931e6cd456202d1c1806cd780afbca3567
antoniojsanchez <antonio.sanchez@idbmobile.com>
2020-07-06 15:03:41 +0200

    Preparing cronjob commands

--- queues/Readers/Vbet/1594040554/0/bookmaker_markets.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO bookmaker_markets (id, fk_bookmaker_id, fk_bookmaker_sport_id, title, skip, outcomes, mapped, created_at) VALUES 
-(DEFAULT, 15, {sport=Football}, 'Team 1 Will Win 1st Half and Won´t Win The Match', 0, '[]', 0, '2020-07-06 00:00:00')
-,(DEFAULT, 15, {sport=Football}, 'Team 2 Will Win 1st Half and Won´t Win The Match', 0, '[]', 0, '2020-07-06 00:00:00')
